b2062a7f5e746383b1502b0dfd8c2e1b7d51734d
agus senes <99297863+agussenes@users.noreply.github.com>
2023-06-06 15:09:21 -0300

    pruebaa

---
+++ indexx.js
@@ -0,0 +1,7 @@
+let nombre = prompt("ingrese su nombre: ")
+let apellido = prompt("ingrese su apellido: ")
+let edad = prompt("ingrese su edad: ")
+
+let resulado = "tu nombre completo es: " + nombre + " " + apellido + " y tu edad es: " + edad
+alert(resulado)
+console.log(resulado)
